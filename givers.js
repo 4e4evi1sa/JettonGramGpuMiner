07ef10cbb9af9291ae4879e0208cd7829533e616
4e4evi1sa <159638599+4e4evi1sa@users.noreply.github.com>
2024-05-18 01:49:03 +0300

    Update givers.js

--- givers.js
+++ givers.js
@@ -14,14 +14,14 @@ exports.givers100 = [
     { address: 'EQBMsmRGXaR5T5TVLFnoMywetN-UfbaiB9NJUgqk2tAm-gSB', reward: 1000 }, // 1000
 ];
 exports.givers1000 = [
-    { address: 'EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2', reward: 1000 },
-    { address: 'EQCvMmHhSYStEtUAEDrpV39T2GWl-0K-iqCxSSZ7I96L4yow', reward: 1000 },
-    { address: 'EQBvumwjKe7xlrjc22p2eLGT4UkdRnrmqmcEYT94J6ZCINmt', reward: 1000 },
-    { address: 'EQDEume45yzDIdSy_Cdz7KIKZk0HyCFIr0yKdbtMyPfFUkbl', reward: 1000 },
-    { address: 'EQAO7jXcX-fJJZl-kphbpdhbIDUqcAiYcAr9RvVlFl38Uatt', reward: 1000 },
-    { address: 'EQAvheS_G-U57CE55UlwF-3M-cc4cljbLireYCmAMe_RHWGF', reward: 1000 },
-    { address: 'EQCba5q9VoYGgiGykVazOUZ49UK-1RljUeZgU6E-bW0bqF2Z', reward: 1000 },
-    { address: 'EQCzT8Pk1Z_aMpNukdV-Mqwc6LNaCNDt-HD6PiaSuEeCD0hV', reward: 1000 },
-    { address: 'EQDglg3hI89dySlr-FR_d1GQCMirkLZH6TPF-NeojP-DbSgY', reward: 1000 },
-    { address: 'EQDIDs45shbXRwhnXoFZg303PkG2CihbVvQXw1k0_yVIqxcA', reward: 1000 }, // 1000
+    { address: 'EQABlpocRxDoECjBtorHZlsgM2VgZQ1UKKgEFKnjeJVKxP-e', reward: 1000 },
+    { address: 'EQBo4OMBbxgWpi5UWSKmwakunaSE9Tbd2xgLks11nEztixtZ', reward: 1000 },
+    { address: 'EQChDqooVTDnsrM1B_GA9ZeOc9pu2GsbWKYS9kC1kJ_k4ZK5', reward: 1000 },
+    { address: 'EQCxZ8ukAahVKy3Dg27IDS0nV05HJWkrd-7Wi-JfC2E6NAj5', reward: 1000 },
+    { address: 'EQB6LBRHsstRCndcD5ZpfgxcEvK91tIPuFbGJdECgepBbYaz', reward: 1000 },
+    { address: 'EQDpaCRCX4pGJTfW5JCmR6XEAkyiy5HvV0PW6D3pilab9tP1', reward: 1000 },
+    { address: 'EQCDs928jTG3HlPp_RXoL8T9frOjnXog_aTyUmjej9mX6G3Q', reward: 1000 },
+    { address: 'EQArQu09ulZPDpGUzkgArORvJskCVsFuCcj_OAUF7Pqv27Q8', reward: 1000 },
+    { address: 'EQB50X6c1LWseWBe28HrG74Iuq7W1DfVxIRkFKNAEohW1QHh', reward: 1000 },
+    { address: 'EQBc5-jYQYylQCvjRhwKC7lm8B10fLBrri1mOWUSPHoaB_1o', reward: 1000 }, // 1000
 ];
